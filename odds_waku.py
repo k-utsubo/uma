3b46ef85188b50af4fb63ed614b497f3f0f88b14
k-utsubo <utsuboka@gmail.com>
2016-06-06 23:08:49 +0900

    add waku

---
+++ odds_waku.py
@@ -0,0 +1,96 @@
+#!/bin/env python
+# coding:utf-8
+
+import urllib
+import urllib2
+from lxml import etree
+import time
+import re
+import datetime
+
+def to_s(s):
+  if s is None:
+    return ""
+  if s == "****":
+    return ""
+  return s.encode('utf-8').strip()
+
+def get_result(code):
+#  try:
+    url="http://keiba.yahoo.co.jp/odds/tfw/"+code+"/"
+    res=urllib.urlopen(url)
+    page=res.read()
+    root=etree.fromstring(page,etree.HTMLParser())
+    get_tanfuku(code,root)
+    time.sleep(1)
+#  except :
+#    print "error:"+code
+
+def to_date(s):
+  s=re.sub('（.*$','',s)
+  s=re.sub('年','/',s)
+  s=re.sub('月','/',s)
+  s=re.sub('日','',s)
+  now=datetime.datetime.strptime(s,"%Y/%m/%d")
+  return now.strftime("%Y-%m-%d")
+  #return s
+  
+
+def get_tanfuku(code,root):
+  elem=root.xpath('//div[@class="clearFix mgnBL"]//table[@class="oddsLs"]')
+  jiku=""
+  waku=""
+  for tbl in elem:
+    for tr in tbl.xpath('./tr'):
+      if len(tr.xpath("./th/div"))!=0:
+        jiku=to_s(tr.xpath("./th/div")[0].text)
+        #print "jiku="+jiku+"\n"
+      elif len(tr.xpath("./th"))!=0:
+        waku=to_s(tr.xpath("./th")[0].text)
+        #print "waku="+waku+"\n"
+      td=tr.xpath('./td')
+      if len(td) != 0:
+        odds=to_s(td[0].text)
+        #print odds+"\n"
+        f=open("data/odds_waku.txt","a")
+        f.write(code+"\t"+waku+"\t"+jiku+"\t"+odds+"\n")
+        f.close()
+
+
+#http://keiba.yahoo.co.jp/race/result/1403010101/
+#14 : 年
+#03 : 競馬場
+#01 : 第何回
+#01 : 第何日
+#01 : レース番号
+#競馬場:
+#01 : 札幌
+#02 : 函館
+#03 : 福島
+#04 : 新潟
+#05 : 東京
+#06 : 中山
+#07 : 中京
+#08 : 京都
+#09 : 阪神
+#10 : 小倉
+
+
+
+def get_resultall():
+  nen=["00","01","02","03","04","05","06","07","08","09","10","11","12","13"]
+  jou=["01","02","03","04","05","06","07","08","09","10"]
+  kai=["01","02","03","04","05","06","07","08","09","10"]
+  nichi=["01","02","03","04","05","06","07","08","09","10"]
+  race=["01","02","03","04","05","06","07","08","09","10","11","12"]
+  for n in nen:
+    for j in jou:
+      for k in kai:
+        for ni in nichi:
+          for r in race:
+            get_result( n+j+k+ni+r )
+
+if __name__ == '__main__':
+  #get_resultall()
+  get_result("1404030211")
+  #get_result("0001010101")
